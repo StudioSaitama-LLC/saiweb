50462896d042a559a4e1a61fb7d63ef1f2cbd0e5
Tomohiro <soeno@studiosaitama.com>
2025-06-16 13:54:44 +0900

    wwwからルートドメインへのリダイレクト設定を再追加

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.studiosaitama.com' }],
+        destination: 'https://studiosaitama.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 export default nextConfig
