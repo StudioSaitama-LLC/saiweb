ebf96299d22efcbdc837cfd325098465bf285b20
Tomohiro <soeno@studiosaitama.com>
2025-06-16 14:05:23 +0900

    リダイレクト設定を指定通りに修正

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.studiosaitama.com',
+          },
+        ],
+        destination: 'https://studiosaitama.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,16 +24,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.studiosaitama.com' }],
-        destination: 'https://studiosaitama.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
